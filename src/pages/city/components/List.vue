ecd77babbecc0569a8b12b87efa340f1f6ef137a
jxmlearner <410958040@qq.com>
2019-01-28 18:29:33 +0800

    城市列表

---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="list">
+        <div class="area">
+            <div class="title border-top1px border-1px">当前城市</div>
+            <div class="button-list">
+                <div class="button-wrapper">
+                    <div class="button">北京</div>
+                </div> 
+            </div>
+        </div>
+        <div class="area">
+            <div class="title border-top1px border-1px">热门城市</div>
+            <div class="button-list">
+                <div class="button-wrapper">
+                    <div class="button">北京</div>
+                </div> 
+            </div>
+        </div>
+        <div class="area">
+            <div class="title border-top1px border-1px">A</div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'CityList'
+    }
+</script>
+
+<style scoped lang="stylus">
+@import '~@/assets/css/stylus/mixins.styl'
+@import '~@/assets/css/stylus/variable.styl'
+
+.title {
+    line-height: .44rem;
+    background: #eee;
+    padding-left: .2rem;
+    color: #666;
+    border-top1px(#ccc)
+    border-1px(#ccc)
+}
+.button-list {
+    overflow: hidden;
+    padding: .1rem;
+    .button-wrapper {
+        float: left;
+        width: 33.33%;
+        .button {
+            margin: .1rem;
+            padding: .1rem 0;
+            text-align: center;
+            border: .01rem solid #ccc;    
+            border-radius: .06rem;
+        }
+    }
+}
+</style>

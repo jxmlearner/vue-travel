6bf48ebde8e4cbb5341fa60a5ac4b2738cf0071a
jxmlearner <410958040@qq.com>
2019-01-29 15:38:54 +0800

    字母表组件的touch联动

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,26 +1,68 @@
 <template>
     <ul class="list">
-        <li class="item" v-for="(item,key) of cities" :key="key" @click="handleLetterChange">{{key}}</li>
+        <li class="item" v-for="item of letters" :key="item" :ref="item"
+        @click="handleLetterChange"
+        @touchstart="handleTouchStart"
+        @touchmove="handleTouchMove"
+        @touchend="handleTouchEnd"
+        >{{item}}</li>
     </ul>
 </template>
 
 <script>
     export default {
         name: 'CityAlphabet',
+        data() {
+            return {
+                touchStatus: false,
+                startY: 0,
+                timer: null
+            }
+        },
         props: {
             cities: Object
+        },
+        computed: {
+            letters() {
+                const letters = Object.keys(this.cities)
+                return letters
+            }
+        },
+        updated() {
+            this.startY = this.$refs['A'][0].offsetTop
         },
         methods: {
             handleLetterChange(e) {
                 let letter = e.target.innerText
                 this.$emit('change',letter)
+            },
+            handleTouchStart() {
+                this.touchStatus = true
+            },
+            handleTouchMove(e) {
+                if(this.touchStatus) {
+                    if(this.timer) {
+                        clearTimeout(this.timer)
+                    }
+                    this.timer = setTimeout(()=>{
+                        const startY = this.startY
+                        const touchY = e.touches[0].clientY - 85
+                        const index = Math.floor((touchY - startY) / 20 )
+                        if(index>=0 && index < this.letters.length){
+                            this.$emit('change',this.letters[index])
+                        }
+                    },16)                                        
+                }
+            },
+            handleTouchEnd() {
+                this.touchStatus = false
             }
         }
     }
 </script>
 
 <style scoped lang="stylus">
-@import '~@/assets/css/stylus/mixins.styl'
+@import '~@/assets/css/stylus/variable.styl'
 .list {
     display: flex;
     flex-direction: column;

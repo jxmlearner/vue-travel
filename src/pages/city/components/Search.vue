8b22d0e97b7706df5d9c18c9eb1013b897f9d368
jxmlearner <410958040@qq.com>
2019-01-29 17:27:05 +0800

    search组件的搜索实现

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,17 +1,67 @@
 <template>
-    <div class="search">
-        <input type="text" class="search-input" placeholder="输入城市名或拼音" />
+    <div>
+        <div class="search">
+            <input type="text" class="search-input" v-model="keyword" placeholder="输入城市名或拼音" />
+        </div>
+        <div class="search-content" ref="search" v-show="keyword">
+            <ul>
+                <li class="search-item border-1px" v-for="item of list" :key="item.id">{{item.name}}</li>
+                <li v-show="hasNoData" class="search-item border-1px">没有找到匹配数据</li>
+            </ul>
+        </div>
     </div>
 </template>
 
 <script>
+    import BScroll from 'better-scroll'
     export default {
-        name: 'CitySearch'
+        name: 'CitySearch',
+        data() {
+            return {
+                keyword: '',
+                list: [],
+                timer: null
+            }
+        },
+        props: {
+            cities: Object
+        },
+        mounted() {
+            this.scroll = new BScroll(this.$refs.search)
+        },
+        computed: {
+            hasNoData() {
+                return !this.list.length
+            }
+        },
+        watch: {
+            keyword() {
+                if(this.timer) {
+                    clearTimeout(this.timer)
+                }
+                if(!this.keyword) {
+                    this.list = []
+                    return
+                }
+                this.timer = setTimeout(()=> {
+                    const result = []
+                    for(let i in this.cities) {
+                        this.cities[i].forEach(value => {
+                            if((value.spell.indexOf(this.keyword)> -1) || (value.name.indexOf(this.keyword)>-1)) {
+                                result.push(value)
+                            }
+                        })
+                    }
+                    this.list = result
+                },100)
+            }
+        }
     }
 </script>
 
 <style scoped lang="stylus">
 @import '~@/assets/css/stylus/variable.styl'
+@import '~@/assets/css/stylus/mixins.styl'
 
 .search {
     padding: .1rem;
@@ -28,6 +78,23 @@
         &:focus {
             outline: none;
         }
+    }
+}
+.search-content {
+    position: absolute;
+    top: 1.7rem;
+    left:0;
+    right: 0;
+    bottom:0;
+    background: #eee;
+    z-index: 1;
+    overflow: hidden;
+    .search-item {
+        line-height: .62rem;
+        padding-left: .2rem;
+        background: #fff;
+        color: #666;
+        border-1px(#ccc)
     }
 }
 </style>
